d53d3283d548a80e9f0484e0170cca3e046a6a39
Diana Ovod <ovod.diana@gmail.com>
2020-10-05 22:05:46 -0700

    added colliders, moving hero and camera
    added character controller
    camera follows the hero

---
+++ Legend of Devslopes Fantasy/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+//when the player moves around, the camera is following along with it
+//reference to the target, the hero
+//tell the camera to move
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float smoothing = 5f;
+
+    Vector3 offset;
+
+    void Awake()
+    {
+        Assert.IsNotNull(target);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //position of the camera minus the position of the player
+        offset = transform.position - target.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetCamePos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamePos, smoothing * Time.deltaTime);
+        //linear interplation between two vectors
+        //starting pos, end pos, and time between them
+
+    }
+}
---
+++ Legend of Devslopes Fantasy/Assets/scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction that we want to go by getting input from the axis
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //moved the character with speed
+        characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+}
