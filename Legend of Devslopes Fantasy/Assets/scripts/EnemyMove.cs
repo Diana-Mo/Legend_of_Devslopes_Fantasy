a7d2586607fca755ede8ca30324598a4681be0d7
Diana Ovod <ovod.diana@gmail.com>
2020-10-07 21:41:37 -0700

    added navmsh Agent, baked the ground
    enemies follow the hero
    defined the area where the enemies can walk (that's called baking apparently)
    enemies are animated now too
    everything is walkable

---
+++ Legend of Devslopes Fantasy/Assets/scripts/EnemyMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(player);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
