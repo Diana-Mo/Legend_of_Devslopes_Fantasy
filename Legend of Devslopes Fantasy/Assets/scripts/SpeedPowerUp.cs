ea9495617942c05919e478ffbc4cf6f133fcecc2
Diana Ovod <ovod.diana@gmail.com>
2020-11-27 20:29:42 -0800

    fire particles
    mostly fire particles, but also beginnings of code

---
+++ Legend of Devslopes Fantasy/Assets/scripts/SpeedPowerUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class SpeedPowerUp : MonoBehaviour
+{
+    private GameObject player;
+    private PlayerController playerController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameManager.Instance.Player;
+        playerController = player.GetComponent<PlayerController>();
+        GameManager.Instance.RegisterPowerUp();
+    }
+
+    void OnTriggerEvent(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerController.SpeedPowerUp();
+            Destroy(gameObject);
+        }
+    }
+}
