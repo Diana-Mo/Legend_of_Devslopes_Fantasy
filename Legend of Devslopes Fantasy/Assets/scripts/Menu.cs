a106f94ecaee503889d321f13c270709170a7c09
Diana Ovod <ovod.diana@gmail.com>
2020-12-21 22:43:48 -0800

    menu animations

--- Legend of Devslopes Fantasy/Assets/scripts/Menu.cs
+++ Legend of Devslopes Fantasy/Assets/scripts/Menu.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] GameObject hero;
+    [SerializeField] GameObject tanker;
+    [SerializeField] GameObject soldier;
+    [SerializeField] GameObject ranger;
+
+    private Animator heroAnim;
+    private Animator tankerAnim;
+    private Animator soldierAnim;
+    private Animator rangerAnim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        heroAnim = hero.GetComponent<Animator>();
+        tankerAnim = tanker.GetComponent<Animator>();
+        soldierAnim = soldier.GetComponent<Animator>();
+        rangerAnim = ranger.GetComponent<Animator>();
+
+        StartCoroutine(showcase());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator showcase()
+    {
+        yield return new WaitForSeconds(1f);
+        heroAnim.Play("SpinAttack");
+        yield return new WaitForSeconds(1f);
+        tankerAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+        soldierAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+        rangerAnim.Play("Attack");
+
+        StartCoroutine(showcase());
     }
 }
